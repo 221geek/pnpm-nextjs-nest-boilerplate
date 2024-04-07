f45790ea3d241b64a1b91b1454b74f7cfbf5d29b
mac <mac@192.168.1.15>
2024-04-07 17:28:51 +0000

    chore: husky setup

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,29 +1,33 @@
-module.exports = {
-	env: {
-		browser: true,
-		es2021: true,
-		node: true,
+module.exports = [
+	{
+		files: [".eslintrc.{js,cjs}", "commitlint.config.js"],
+		rules: {
+			"no-unused-vars": "off",
+			"@typescript-eslint/no-unused-vars": "off",
+		},
+		languageOptions: {
+			sourceType: "script",
+			globals: {},
+		},
 	},
-	extends: "standard-with-typescript",
-	overrides: [
-		{
-			env: {
-				node: true,
-			},
-			files: [".eslintrc.{js,cjs}", "commitlint.config.js"],
-			rules: {
-				"no-unused-vars": "off",
-				"@typescript-eslint/no-unused-vars": "off",
-			},
-			parserOptions: {
-				sourceType: "script",
+	{
+		files: ["*.ts", "*.tsx"],
+		languageOptions: {
+			ecmaVersion: "latest",
+			sourceType: "module",
+			parser: "@typescript-eslint/parser",
+			project: "./tsconfig.eslint.json",
+			globals: {
+				window: "readonly",
+				document: "readonly",
+				process: "readonly",
+				global: "readonly",
 			},
 		},
-	],
-	parserOptions: {
-		ecmaVersion: "latest",
-		sourceType: "module",
-		project: "./tsconfig.eslint.json",
+		linterOptions: {
+			extends: "standard-with-typescript",
+			plugins: ["@typescript-eslint"],
+		},
+		rules: {},
 	},
-	rules: {},
-};
+];
